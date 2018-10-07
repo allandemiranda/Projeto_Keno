d2b93c92370d09dcba6cbba6e364aad0c7f87062
Allan de Miranda <allandemiranda@gmail.com>
2018-10-07 16:13:13 -0300

    payout_table adicionado

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,55 @@
 
 #include "../include/KenoBet.h"
 
+// Global vector
+std::vector < std::vector <float> > payout_table = {
+    {
+        0, 3
+    },
+    {
+        0, 1, 9
+    },
+    {
+        0, 1, 2, 16
+    },
+    {
+        0, 0.5, 2, 6, 12
+    },
+    {
+        0, 0.5, 1, 3, 15, 50
+    },
+    {
+        0, 0.5, 1, 2, 3, 30, 75
+    },
+    {
+        0, 0.5, 0.5, 1, 6, 12, 36, 100
+    },
+    {
+        0, 0.5, 0.5, 1, 3, 6, 19, 90, 720
+    },
+    {
+        0, 0.5, 0.5, 1, 2, 4, 8, 20, 80, 1200 
+    },
+    {
+        0, 0, 0.5, 1, 2, 3, 5, 10, 30, 600, 1800
+    },
+    {
+        0, 0, 0.5, 1, 1, 2, 6, 15, 25, 180, 1000, 3000
+    },
+    {
+        0, 0, 0, 0.5, 1, 2, 4, 24, 72, 250, 500, 2000, 4000
+    },
+    {
+        0, 0, 0, 0.5, 0.5, 3, 4, 5, 20, 80, 240, 500, 3000, 6000
+    },
+    {
+        0, 0, 0, 0.5, 0.5, 2, 3, 5, 12, 50, 150, 500, 1000, 2000, 7500
+    },
+    {
+        0, 0, 0, 0.5, 0.5, 1, 2, 5, 15, 50, 150, 300, 600, 1200, 2500, 10000
+    }
+};
+
 /**
  * @brief Function to get base file parameters
  * 
@@ -57,7 +106,7 @@ bool initialization_parameters(char *FileName, KenoBet &fist){
     //fist.set_wage(parameters[0]); // Sets the amount of money the player is betting
     //fist.rounds(parameters[1]); ONDE ESTÀ O PARÃMETRO DAS RODADAS ???
     for(int i(2); i<(parameters.size()); ++i){
-        //fist.add_number((short int)(parameters[i]));
+        //fist.add_number((unsigned short int)(parameters[i]));
     } 
     return true;
 }
@@ -83,10 +132,16 @@ int main( int argc, char **argv )
      * @brief Initial Detail
      * 
      */
-    std::cout << "You are going to wage a total of $1500 dollars." << std::endl;
-    std::cout << "Going for a total of 3 rounds, waging $500 per round." << std::endl;
+    std::cout << "You are going to wage a total of $" << /*player_bet.m_wage() [pegar quantidade de dienhiro ??]  << */ " dollars." << std::endl;
+    std::cout << "Going for a total of " << /*player_bet.roud() [pegar rodadas ??]  << */ " rounds, waging $" << /*player_bet.m_wage()/player_bet.roud() [??]  << */ " per round." << std::endl;
     std::cout << std::endl;
-    std::cout << "Your bet has 3 numbers. They are: [ 12 21 64 ]" << std::endl;
+    std::cout << "Your bet has " << /*player_bet.m_spots.size() << */" numbers. They are: [ ";
+    /*
+    for(unsigned short int i : player_bet.m_spots){
+        std::cout << i << " ";
+    }
+    */
+    std::cout << "]" << std::endl;
 
     return EXIT_SUCCESS;
     
